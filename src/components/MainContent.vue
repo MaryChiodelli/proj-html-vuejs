30b15289a2e884a6a074571ca5ee2afc4379c9e3
MaryChiodelli <marychiodelli97@gmail.com>
2022-09-14 18:13:05 +0200

    add hero structure

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,5 +1,21 @@
 <template>
-  <main>Content</main>
+  <main>
+    <section class="hero">
+      <div class="container">
+        <div class="row">
+          <div class="col w-50">
+            <div>I'm wingman</div>
+            <h1>Peace On Earth A Wonderful Wish But No Way</h1>
+            <p>Success isn't overnight. It's when everyday you get a little better than the day before. It all adds up.</p>
+            <a href="#">Get Started For Free</a>
+          </div>
+          <div class="col w-50">
+            <img src="../assets/home-movation-hero-image.png" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
@@ -9,5 +25,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  .hero {
+    padding-top: 80px;
+    margin-top: -80px;
+    background-image: url('../assets/background-pattern-wavify.png');
 
+    .container {
+      padding-top: 125px;
+    }
+  }
+
+  .w-50 {
+    width: 50%;
+  }
 </style>
